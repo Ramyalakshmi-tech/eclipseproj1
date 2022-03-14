287cb7f0ee37cdc2645ed299ee5c57bee8939827
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-14 12:33:10 +0530

    m

---
+++ src/com/add/addition.java
@@ -0,0 +1,16 @@
+package com.add;
+
+public class addition {
+	public static void main(String[] args) {
+	String name="Ramya";
+	Integer a=2;
+	System.out.println(++a);
+	if(a%2==0) {
+		System.out.println("Even");
+	}
+	else {
+		System.out.println("Odd");
+	}
+	}
+
+}
